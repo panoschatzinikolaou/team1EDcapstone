444347a250f2477e1fc3ebb433135971b72445fe
panagiotis.chatzinikolaou <panagiotis.chatzinikolaou@eurodyn.com>
2022-04-19 10:01:51 +0300

    Initial commit

---
+++ src/main/java/com/edteam1/smdb/Team1EDcapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.edteam1.smdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1EDcapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team1EDcapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edteam1/smdb/Team1EDcapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edteam1.smdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1EDcapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
